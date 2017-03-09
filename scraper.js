6c12af64cf87b212f2b1e243222e03423b92a4f7
Jan Oldrich Kruza <sixtease+github@gmail.com>
2017-03-09 22:33:05 +0100

    stahování PE-indexu

--- scraper.js
+++ scraper.js
@@ -1,49 +1,40 @@
 'use strict';
 
-var request = require('request');
+var fs = require('fs');
+var request = require('request-promise');
 var cheerio = require('cheerio');
 
-var tickers = [];
+// Načtení seznamu Tickerů ze souboru
+var tickers = JSON.parse(fs.readFileSync('tickers_map_sample.json'));
+var list_of_tickers = Object.keys(tickers);
 
-var get_tickers_page_url = 'http://finviz.com/screener.ashx?v=111';
+var requests = [];
 
-var request_options = {
-    url: get_tickers_page_url,
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-    },
-};
+// Stažení PE_RATIO-value hodnot pro dné tickery
+list_of_tickers.forEach(function (index) { 
+    var request_options = {
+        uri: 'http://finance.yahoo.com/quote/'+index+'?p='+index,
+        headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',},
+    };
+      
+    requests.push(
+        request(request_options).then(function (html) {
+            var $ = cheerio.load(html);
+            var p = $("td[data-test='PE_RATIO-value']").text();
+            tickers[index].pe = p;
+        })
+    );
+});
 
-function request_tickers_page (url) {
-    request_options.url = url;
-    request(request_options, function (error, response, html) {
-        var $ = cheerio.load(html);
-        extract_tickers($, tickers);
-        maybe_load_next_tickers_page($);
-    });
-}
+Promise.all(requests).then(function () {
+    var dateFormat = require('dateformat');
+    var now = new Date();
+    var d = dateFormat(now, "yyyy-mm-dd");
+    var csv_lines = [];
 
-function extract_tickers($, tickers) {
-    $('.screener-link-primary').each(function () {
-        var ticker = $(this).text();
-        tickers.push(ticker);
-        console.log(ticker);
-    });
-}
-
-function maybe_load_next_tickers_page($) {
-    var $next_page_option = $('#pageSelect>[selected]+option');
-    var next_page_number;
-    if ($next_page_option && (next_page_number = $next_page_option.val())) {
-        request_tickers_page(get_tickers_page_url + '&r=' + next_page_number);
-    }
-    else {
-        tickers_loaded();
-    }
-}
-
-function tickers_loaded() {
-    console.log('done');
-}
-
-request_tickers_page(get_tickers_page_url);
+    // ADD TO EXISTING CSV FILEs ROW BY ROW   node test_2.js
+    list_of_tickers.forEach(function (i) {
+        var ticker = tickers[i];
+        fs.appendFileSync(i+".csv", [d, i, ticker.name, ticker.pe].join(';')  + '\r\n');
+    })
+});
